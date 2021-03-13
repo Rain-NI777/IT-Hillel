3f94f94edcdfa7eeed9952d9efc6786d1ffce5eb
Rain-NI777 <80512559+Rain-NI777@users.noreply.github.com>
2021-03-13 14:22:44 +0200

    Update homework.sh

--- homework.sh
+++ homework.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 chmod +x homework.sh
+
 python -m venv projectx
+source ./projectx/bin/activate
+pip install flask
+pip freeze > requirements.txt
 
 echo "Hello! My name is Rain"
 echo "This is my first project on Bash"
